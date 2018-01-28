36bbfe325bc7a77c7ab65e4fc6b6c4950f653fec
Robert Lego <robertlego@gmail.com>
2018-01-28 03:24:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
+function nowServing(deliLine) {
+  var customer = deliLine[0];
+  deliLine.shift();
+  return customer;
+}
